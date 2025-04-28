60cc8c93e4c64b475fb004f8f3366b5d784702d4
JaimeLomo <jaime.lomo@alumnos.uneatlantico.es>
2025-04-28 09:53:33 +0200

    Commit de todos los programas

---
+++ inditex.js
@@ -0,0 +1,10 @@
+const express= require(`express`);
+const app = express;
+const port = 3000;
+
+app.length(`/`, (req,res) => {
+    res.send('¡Hola desde la nube con Express');
+});
+app.listen(port, () => {
+    console.log('Servidor escuchando en http://localhost:$(port})')
+});
